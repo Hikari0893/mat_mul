bd9caf3e7540b97746e6ff8ef349a4fe268114ed
Hikari0893 <dayana.pparra@outlook.com>
2023-04-18 15:58:12 -0600

    Mem_manager working
    Co-authored-by: Tonix22 <emiliotonix@gmail.com>

--- Mem_Manager.v
+++ Mem_Manager.v
@@ -1,37 +1,46 @@
+`include "macro.v"
+
 module Mem_Manager
 (
     input clk,
     input rst,
-	output reg [2:0] Dir_M1,
-    output reg [6:0] Dir_M2
+	output reg [(`ADDR_BITS - 1):0] Dir_M1,
+    output reg [(`ADDR_BITS - 1):0] Dir_M2
     
 );
 
-reg [6:0] count;
+reg [(`ADDR_BITS - 1):0] count;
+reg [(`ADDR_BITS - 1):0] slowcount;
 
 always @(posedge clk or negedge rst) 
 begin
     if (!rst) 
     begin
-        count = 7'd0;
-        Dir_M1 = 3'd0;
+        count     = 7'd0;
+        slowcount = 7'd0;
+        Dir_M1    = 7'd0;
+        Dir_M2    = 7'd0;
     end
-    else if (count == 7'd64)
+    else if (count == 7'd56)
     begin
-        count = 7'd0;
+        count  = 7'd0;
         Dir_M2 = 7'd0;
-        if (Dir_M1 == 3'd7) 
+        //slow
+        if (slowcount == 7'd56) 
         begin
-            Dir_M1 = 3'd0;
-        end 
+            slowcount = 7'd0;
+            Dir_M1    = 7'd0;
+
+        end
         else 
         begin
-            Dir_M1 = Dir_M1 + 1'd1;
+            slowcount = slowcount + 7'd8;
+            Dir_M1    = slowcount;
         end
     end 
     else 
     begin
-        count = count + 1'd1;
+        count  = count + 7'd8;
         Dir_M2 = count;
     end
 end
---
+++ Mem_manager_tb.v
@@ -0,0 +1,38 @@
+`include "macro.v"
+`timescale 10ns / 1ns
+
+module Mem_manager_tb();
+
+    reg clk_tb;
+    reg rst_tb;
+    wire [(`ADDR_BITS - 1):0] Dir_M1_tb;
+    wire [(`ADDR_BITS - 1):0] Dir_M2_tb;
+
+    Mem_Manager DUT (
+        .clk(clk_tb),
+        .rst(rst_tb),
+        .Dir_M1(Dir_M1_tb),
+        .Dir_M2(Dir_M2_tb)
+    );
+
+    initial begin
+        clk_tb = 0;
+
+        rst_tb = 1;
+		  
+        #10
+        rst_tb = 0;
+        #10
+        rst_tb = 1;
+        #1000;
+
+        // Test case 1
+        #100;
+        #1000;
+        $stop;
+    end
+
+    always #5 clk_tb = ~clk_tb;
+
+endmodule
+
