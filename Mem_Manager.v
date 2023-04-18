822b80909357e09c6d80753b9036af74275ca100
tonix22 <emiliotonix@gmail.com>
2023-04-18 15:41:18 -0500

    Modify to blocking

--- Mem_Manager.v
+++ Mem_Manager.v
@@ -13,26 +13,26 @@ always @(posedge clk or negedge rst)
 begin
     if (!rst) 
     begin
-        count <= 7'd0;
-        Dir_M1 <= 3'd0;
-    end 
-    else if (count == 7'd64) 
+        count = 7'd0;
+        Dir_M1 = 3'd0;
+    end
+    else if (count == 7'd64)
     begin
-        count <= 7'd0;
-        Dir_M2 <= 7'd0;
+        count = 7'd0;
+        Dir_M2 = 7'd0;
         if (Dir_M1 == 3'd7) 
         begin
-            Dir_M1 <= 3'd0;
+            Dir_M1 = 3'd0;
         end 
         else 
         begin
-            Dir_M1 <= Dir_M1 + 1'd1;
+            Dir_M1 = Dir_M1 + 1'd1;
         end
     end 
     else 
     begin
-        count <= count + 1'd1;
-        Dir_M2 <= count;
+        count = count + 1'd1;
+        Dir_M2 = count;
     end
 end
   
