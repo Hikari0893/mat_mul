2e3a529fd4d737ff864908123b362b642b99fe8b
Hikari0893 <dayana.pparra@outlook.com>
2023-04-19 17:18:12 -0600

    code of blok select

--- Block_Select.v
+++ Block_Select.v
@@ -1,47 +1,41 @@
-`include "../macro.v"
+`include "macro.v"
 
 
 module Block_Select 
 
 (
-    input clk,
+    input state,
     input [((`WORD_LEN*`MATRIX_DIM)-1):0] Br_m1,
     input [((`WORD_LEN*`MATRIX_DIM)-1):0] Br_m2,
     input [((`WORD_LEN*`MATRIX_DIM)-1):0] Bi_m1,
-    input [((`WORD_LEN*`MATRIX_DIM)-1):0] Bi_m2,
+    input [((`WORD_LEN*`MATRIX_DIM)-1):0] Bi_m2
 
-    output [31:0] Block_0,
-    output [31:0] Block_1,
-    output [31:0] Block_2,
-    output [31:0] Block_3
+    output [((`WORD_LEN*`MATRIX_DIM)-1):0] MX_1,
+    output [((`WORD_LEN*`MATRIX_DIM)-1):0] MX_2,
+    output [((`WORD_LEN*`MATRIX_DIM)-1):0] MY_1,
+    output [((`WORD_LEN*`MATRIX_DIM)-1):0] MY_2
 );
 
-// Estados de la FSM
-    localparam [1:0] REAL = 2'b00, IMAG = 2'b01;
+always @(*) begin
 
-//Señales de estado y de salida
-    reg [1:0] actual_state, next_state; 
-    reg [63:0] M1_M2_Real_siguiente, M1_M2_Imag_siguiente;    
+    if state == 'REAL_SET begin
+        MX_1 <= Br_m1;
+        MX_2 <= Br_m2;
+        MY_1 <= Bi_m1;
+        MY_2 <= Bi_m2;
+    else begin // Imaginay part crossed
+        MX_1 <= Br_m1;
+        MX_2 <= Bi_m1;
+        MY_1 <= Br_m2;
+        MY_2 <= Bi_m2;
+    end
 
-
-//Lógica del estado siguiente 
-always @(*)
-begin
-
-
-
-
-end    
-
-//Lógica de salida
- always @(posedge clk) 
- begin 
+end
 
 
+    
+end
 
-
-
- end
 
 
 endmodule
