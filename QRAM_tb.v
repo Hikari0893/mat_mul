1a6189c9b0e45440827f9049580e2bfa5f35a959
Hikari0893 <dayana.pparra@outlook.com>
2023-04-19 16:36:01 -0600

    QRAM working
    Co-authored-by: Tonix22 <emiliotonix@gmail.com>

--- QRAM_tb.v
+++ QRAM_tb.v
@@ -7,7 +7,7 @@ module QRAM_tb();
 
 // Entradas
 reg [(`ADDR_BITS - 1):0] Dir_M1, Dir_M2; // direccion de memoria
-reg we;
+reg [3:0]we;
 reg src_clk;
 reg [(`WORD_LEN-1):0] data_m1_real,data_m1_imag,data_m2_real,data_m2_imag; // Necesario para escribir 
 
@@ -44,7 +44,10 @@ integer fd_B; // file handler
 integer scan_file; // file handler
 integer i;
 integer j;
-reg [(`WORD_LEN-1):0] captured_data;
+reg [(`WORD_LEN-1):0] captured_data_0;
+reg [(`WORD_LEN-1):0] captured_data_1;
+reg [(`WORD_LEN-1):0] captured_data_2;
+reg [(`WORD_LEN-1):0] captured_data_3;
 
 initial begin
 
@@ -66,91 +69,102 @@ initial begin
     end
 
 
-    we = 1'b1; // write enable
+    we = 4'b0000; // write disable
     // intial memory address
     Dir_M1 = 7'h00;
     
     for(i = 0;i < 1;i=i+1) //! Realizacion
     begin
 
+        we = 4'b0001; // REAL MATRIX enable
         /***************************************
         ************ MATRIX A *******************
         *****************************************/
         //RESET THE ADDRES FOR WRITE
         Dir_M1 = 0;
 
-        scan_file = $fscanf(fd_A, "%s\n", captured_data);//float line
+        scan_file = $fscanf(fd_A, "%s\n", captured_data_0);//float line
         // Read real part
         for (j = 0; j < 64; j = j + 1) begin
-            scan_file = $fscanf(fd_A, "%X,", captured_data);//row number
+            scan_file = $fscanf(fd_A, "%X,", captured_data_0);//row number
             // Do something with each item in captured_data
-            $display("captured_data[%d] = %X", j, captured_data);
+            $display("captured_data_0[%d] = %X", j, captured_data_0);
             #3
-            data_m1_real = captured_data; //! REAL
+            data_m1_real = captured_data_0; //! REAL
             #2
             Dir_M1 = Dir_M1+1;
             #10;
         end
 
+        #10
+        we = 4'b0100; // IMAG MATRIX enable
+
         //RESET THE ADDRES FOR WRITE
-        Dir_M1 = 0;
+        Dir_M1 = 7'h00;
 
         // Read imaginary part
-        for (j = 64; j < 128; j = j + 1) begin
-            scan_file = $fscanf(fd_A, "%X,", captured_data);//row number
+        for (j = 0; j < 64; j = j + 1) begin
+            scan_file = $fscanf(fd_A, "%X,", captured_data_1);//row number
             // Do something with each item in captured_data
-            $display("captured_data[%d] = %X", j, captured_data);
+            $display("captured_data_1[%d] = %X", j+64, captured_data_1);
             #3
-            data_m1_imag = captured_data; //! IMAG
+            data_m1_imag = captured_data_1; //! IMAG
             #2
             Dir_M1    = Dir_M1+1;
             #10;
         end
 
+        #10
+        we = 4'b0010; // REAL MATRIX enable
+
         /***************************************
         ************ MATRIX B *******************
         *****************************************/
         Dir_M2 = 0;
-        scan_file = $fscanf(fd_B, "%s\n", captured_data);//float line
+        scan_file = $fscanf(fd_B, "%s\n", captured_data_2);//float line
         // Read real part
         for (j = 0; j < 64; j = j + 1) begin
-            scan_file = $fscanf(fd_B, "%X,", captured_data);//row number
+            scan_file = $fscanf(fd_B, "%X,", captured_data_2);//row number
             // Do something with each item in captured_data
-            $display("captured_data[%d] = %X", j, captured_data);
+            $display("captured_data_2[%d] = %X", j, captured_data_2);
             #3
-            data_m2_real = captured_data; //! REAL
+            data_m2_real = captured_data_2; //! REAL
             #2
             Dir_M2 = Dir_M2+1;
             #10;
         end
 
+        #10
+        we = 4'b1000; // REAL MATRIX enable
+
+        
         //RESET THE ADDRES FOR WRITE
         Dir_M2 = 0;
 
         // Read imaginary part
         for (j = 64; j < 128; j = j + 1) begin
-            scan_file = $fscanf(fd_B, "%X,", captured_data);//row number
+            scan_file = $fscanf(fd_B, "%X,", captured_data_3);//row number
             // Do something with each item in captured_data
-            $display("captured_data[%d] = %X", j, captured_data);
+            $display("captured_data[%d] = %X", j, captured_data_3);
             #3
-            data_m2_imag = captured_data; //! IMAG
+            data_m2_imag = captured_data_3; //! IMAG
             #2
             Dir_M2    = Dir_M2+1;
             #10;
         end
+        
     end
-    $fclose(fd_A);
-
-
+    #100
     // start reading sequence
-    we     = 1'b0;
+    we = 4'b0000;
+    #10;
     Dir_M1 = 7'h00;
     Dir_M2 = 7'h00;
-	 
-	 	 
-    #10000;
-	 $fclose(fd_A);
-	 $fclose(fd_B);
+    #1000
+    $stop;
+
+	$fclose(fd_A);
+	$fclose(fd_B);
     // parallel read testing
     $stop;
 end
--- Q_RAM.v
+++ Q_RAM.v
@@ -1,54 +1,57 @@
-`include "../macro.v"
+`include "macro.v"
 
 module Q_RAM 
 (
-    input we, clk,
-    input [(`WORD_LEN-1):0] data_m1,data_m2,
-    input [(`ADDR_BITS - 1):0] Dir_M1, Dir_M2,
-
+    input [3:0]we, 
+    input clk,
+    input [(`WORD_LEN-1):0] data_m1_real,data_m1_imag,data_m2_real,data_m2_imag,   // Necesario para escribir
+    input [(`ADDR_BITS - 1):0] Dir_M1, Dir_M2, // direccion de memoria
     output [((`WORD_LEN*`MATRIX_DIM)-1):0] Br_m1,
     output [((`WORD_LEN*`MATRIX_DIM)-1):0] Br_m2,
     output [((`WORD_LEN*`MATRIX_DIM)-1):0] Bi_m1,
     output [((`WORD_LEN*`MATRIX_DIM)-1):0] Bi_m2
 
-
 );
+
 
 single_port_ram M1_Real
 (
-    .data (data_m1),
+    .data (data_m1_real),
     .addr (Dir_M1),
-    .we   (we),
+    .we   (we[0]),
     .clk  (clk),
     .q    (Br_m1)
 );
 
-single_port_ram M1_Imag
-(
-    .data (data_m1),
-    .addr (Dir_M1),
-    .we   (we),
-    .clk  (clk),
-    .q    (Bi_m1)
-);
-
 single_port_ram M2_Real
 (
-    .data (data_m2),
+    .data (data_m2_real),
     .addr (Dir_M2),
-    .we   (we),
+    .we   (we[1]),
     .clk  (clk),
     .q    (Br_m2)
 );
 
+single_port_ram M1_Imag
+(
+    .data (data_m1_imag),
+    .addr (Dir_M1),
+    .we   (we[2]),
+    .clk  (clk),
+    .q    (Bi_m1)
+);
+
+
 single_port_ram M2_Imag
 (
-    .data (data_m2),
+    .data (data_m2_imag),
     .addr (Dir_M2),
-    .we   (we),
+    .we   (we[3]),
     .clk  (clk),
     .q    (Bi_m2)
 );
+
+
 
 
 endmodule
